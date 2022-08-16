ebef7fa9a9bd74ead8bf3201c9627ae3da703017
esha-man <esh8284@gmail.com>
2022-08-17 00:06:47 +0300

    middleware check user-role

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,9 +1,10 @@
 const Router = require("express");
 const router = new Router();
 const typeController = require("../controllers/typeController")
+const checkRole = require("../middleware/checkRoleMiddleware")
 
 
-router.post("/", typeController.create);
+router.post("/", checkRole("admin"), typeController.create);
 router.get("/", typeController.getAll);
 // router.put("/", typeController.put);
 // router.delete("/", typeController.delete);
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,11 +2,11 @@ const Router = require("express");
 const {request} = require("express");
 const router = new Router();
 const userController = require("../controllers/userController")
-
+const authMiddleware = require("../middleware/authMiddleware")
 
 router.post("/registration", userController.registration);
 router.post("/login", userController.login);
-router.get("/auth", userController.checkRegistration);
+router.get("/auth", authMiddleware, userController.checkRegistration);
 // router.put("/");
 // router.delete("/");
 
